3ce4c8db2b12010597f32b870538452ba9de02c0
alex_0ff <35507235+alexandrecoulay@users.noreply.github.com>
2022-09-30 11:58:40 +0200

    Websocket routes update

--- src/Permissions/WebSocket.ts
+++ src/Permissions/WebSocket.ts
@@ -1,7 +1,10 @@
 export const WebSocketRoutes = {
   CONNECT: 0,
   CHECK_CONNECTION: 1,
-  SEND_MESSAGE: 2
+  SEND_MESSAGE: 2,
+  READ_MESSAGE: 3,
+  START_TYPING: 4,
+  STOP_TYPING: 5
 }
 
 export default WebSocketRoutes;
