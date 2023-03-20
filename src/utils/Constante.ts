9683824cac135d64c8c5beb536505076a5321194
alex_0ff <35507235+alexandrecoulay@users.noreply.github.com>
2023-03-20 18:48:39 +0100

    Update Constante.ts

--- src/utils/Constante.ts
+++ src/utils/Constante.ts
@@ -1,3 +1,3 @@
 export const cdnsiteurl = 'https://cdn.trenderapp.com';
-export const baseapiurl = 'https://api.trenderapp.com/api';
-export const websocketurl = 'wss://api.trenderapp.com';
+export const baseapiurl = 'https://api.trenderapp.com/api/v2';
+export const websocketurl = 'wss://api.trenderapp.com/api/v2';
