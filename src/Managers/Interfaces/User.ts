cdf96dfee6cab2a1b6af531cfc0f28d5745bb148
alex_0ff <61584867+alex0ff@users.noreply.github.com>
2023-06-11 23:32:08 +0200

    add user profile info

--- src/Managers/Interfaces/User.ts
+++ src/Managers/Interfaces/User.ts
@@ -1,34 +1,36 @@
-import type { error, premium_type } from "./Global"
+import type { error, premium_type } from "./Global";
 
 export interface userInfo {
-    user_id: string,
-    username: string,
-    nickname: string,
-    description: string,
-    premium_type: premium_type,
-    avatar: string,
-    banner: string,
-    link?: string,
-    accent_color: string,
-    flags: number,
-    is_private: boolean,
-    allow_dm: boolean,
-    pined_post?: string,
-    created_at: Date,
-    trust_factor: number
+    user_id: string;
+    username: string;
+    nickname: string;
+    description: string;
+    premium_type: premium_type;
+    avatar: string;
+    banner: string;
+    link?: string;
+    accent_color: string;
+    flags: number;
+    is_private: boolean;
+    allow_dm: boolean;
+    pined_post?: string;
+    created_at: Date;
+    trust_factor: number;
+    certified: boolean;
 }
 
 export interface profileInformationsInterface {
-    user_info: userInfo,
+    user_info: userInfo;
     subscriptions?: {
-        total: number
-    },
+        total: number;
+    };
     subscribers?: {
-        total: number
-    }
+        total: number;
+    };
+    following: boolean;
 }
 
 export interface profileInformations {
-    error?: error,
-    data?: profileInformationsInterface
+    error?: error;
+    data?: profileInformationsInterface;
 }
