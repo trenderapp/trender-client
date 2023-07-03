992c83b17a0fa663786b821ff09fa0102f47c27a
alex_0ff <61584867+alex0ff@users.noreply.github.com>
2023-07-03 21:59:55 +0200

    Update Affiliation.ts

--- src/Managers/Interfaces/Affiliation.ts
+++ src/Managers/Interfaces/Affiliation.ts
@@ -1,8 +1,9 @@
 import type { error } from "./Global"
 
 export interface affiliationFetchSchema {
-    my_code?: string,
-    affiliate_to?: string
+    my_code?: string;
+    affiliate_to?: string;
+    affiliate_number: number;
 }
 
 export interface affiliationCreateResponse {
