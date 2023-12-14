7a498a100efdb1387b50f0dedfeae84d8a36efcd
alex_0ff <35507235+alexandrecoulay@users.noreply.github.com>
2023-12-14 17:41:04 +0100

    Update follow interface

--- src/Managers/Interfaces/Follow.ts
+++ src/Managers/Interfaces/Follow.ts
@@ -1,25 +1,26 @@
-import type { error } from "./Global"
+import type { error } from "./Global";
 
 export interface followInformations {
-    error?: error
+    error?: error;
 }
 
 export interface followInformationsResponse {
-    follow_id: string, 
-    user_id: string, 
-    target_id: string, 
-    created_at: Date,
-    description?: string,
-    accepted: boolean, 
-    nickname: string, 
-    username: string, 
-    avatar: string, 
-    is_private: boolean, 
-    verified: boolean
+    follow_id: string;
+    flags: number;
+    user_id: string;
+    target_id: string;
+    created_at: Date;
+    description?: string;
+    accepted: boolean;
+    nickname: string;
+    username: string;
+    avatar: string;
+    is_private: boolean;
+    verified: boolean;
 }
 
 export interface followListInformations {
-    error?: error,
-    data?: Array<followInformationsResponse>,
-    pagination_key?: string
+    error?: error;
+    data?: Array<followInformationsResponse>;
+    pagination_key?: string;
 }
